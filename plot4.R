a444029b0e028b2c6604e46db3631cdf0489b52e
Megha-Sankar <64921897+Megha-Sankar@users.noreply.github.com>
2020-07-01 16:51:45 +0530

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,15 @@
+#Q4: Across the United States, how have emissions from coal 
+## combustion-related sources changed from 1999-2008
+
+SCC_sub <- SCC[grepl("Coal" , SCC$Short.Name), ]
+NEI_sub <- NEI[NEI$SCC %in% SCC.sub$SCC, ]
+
+png("Plot4.png",width=400,height=400,units="px",bg="transparent")
+
+ggp <- ggplot(NEI_sub, aes(x = factor(year), y = Emissions, fill =type)) + 
+geom_bar(stat= "identity", width = .4) + xlab("year") +
+ylab("Coal-Related PM2.5 Emissions") + 
+ggtitle("Total Coal-Related PM2.5 Emissions")
+
+print(ggp)
+dev.off() 
