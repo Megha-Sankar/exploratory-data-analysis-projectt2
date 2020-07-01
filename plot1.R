a5d41b7edc1afd2db5f1f9c619566f73fc16b155
Megha-Sankar <64921897+Megha-Sankar@users.noreply.github.com>
2020-07-01 16:26:37 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,14 @@
+## TO read in emissions data and classification code
+data_emissions <- readRDS("summarySCC_PM25.rds")
+class_code <- readRDS("Source_Classification_Code.rds")
+
+## add up the total emissions for each year
+sum_by__year <- aggregate(emissions_data$Emissions, by=list(year=emissions_data$year), FUN=sum)
+
+## create the plot
+png(filename = "plot1.png")
+plot(sum_by_year$year, sum_by_year$x, type = "l", 
+     main = "Total Emissions of PM2.5 in Baltimore City",
+     ylab = "Total emissions of PM2.5 (tons)",
+     xlab = "Year")
+dev.off()
